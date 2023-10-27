71503af70b4095687bafe8001b4df4e5b0d3eca0
MauricioTejada <mauriciotejada14@gmail.com>
2023-10-26 22:47:59 -0300

    first commit

--- pages/about.jsx
+++ pages/about.jsx
@@ -3,23 +3,23 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
-export default function Home() {
+export default function AbouPage() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Home - Mauricio</title>
-        <meta name="description" content="Home Page" />
+        <title>About - Mauricio</title>
+        <meta name="description" content="About Page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Ir a <Link href="/about">About</Link>
+          Ir a <Link href="/">Home</Link>
         </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <code className={styles.code}>pages/about.jsx</code>
         </p>
       </main>
 
